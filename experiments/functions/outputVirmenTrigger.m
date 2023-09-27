e0b6841fca0d9008d69d0fb7c021b97ebd591bd6
xy103 <xintong.cindy.yuan@gmail.com>
2023-09-27 17:07:05 -0400

    marked outputVirmenTrigger as deprecated

--- experiments/functions/outputVirmenTrigger.m
+++ experiments/functions/outputVirmenTrigger.m
@@ -1,3 +1,6 @@
+% CY 9/27/2023: Deprecated old function for sync'ing. Replaced by
+% outputVirmenHighLow
+
 function [vr] = outputVirmenTrigger(vr)
 outputSingleScan(vr.dio,[1]);
 outputSingleScan(vr.dio,[0]);
