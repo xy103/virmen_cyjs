2ad1362cd39efce41921d1608ef7c9554ac514e0
xy103 <xintong.cindy.yuan@gmail.com>
2023-09-27 14:32:46 -0400

    cleaned up initOpto_CY_SW, separated ramp up and down duration variables, and ensured all manual variables are initialized

--- experiments/functions/initOpto_CY_SW.m
+++ experiments/functions/initOpto_CY_SW.m
@@ -1,4 +1,5 @@
 function vr = initOpto_CY_SW(vr)
+% initialization function for 
 
 vr.optoOn = 0; % initialize opto indicator to be 0 (change to 1 when opto is on) 
 vr.optoOnSec = 0; % keep at 0 at init
@@ -7,23 +8,14 @@ vr.trialOptoVar = 0; % initialize random variable for probabilistic opto deliver
 
 vr.optoMaxVoltage = eval(vr.exper.variables.optoMaxVoltage); % max voltage value
 vr.optoThreshold = eval(vr.exper.variables.optoThreshold); % probability/threshold for opto stimulation
-% vr.optoMinVoltage = eval(vr.exper.variables.optoMinVoltage); % min voltage value
-% vr.optoVoltageIncrement = eval(vr.exper.variables.optoVoltageIncrement); % increment value
-vr.optoRampDur = eval(vr.exper.variables.optoRampDur); % ramp on and off duration in sec 
+
+vr.optoRampUpDur = eval(vr.exper.variables.optoRampDur); % ramp on duration in sec 
+vr.optoRampDownDur = eval(vr.exper.variables.optoRampDur); % ramp off duration in sec 
 vr.optoLightDur = eval(vr.exper.variables.optoLightDur); % sustained light duration in sec 
-% vr.optoRepeat = eval(vr.exper.variables.optoRepeat); % how many repetition per light level
-% vr.minIterBetweenOpto = eval(vr.exper.variables.minIterBetweenOpto); % how many iteration to wait until giving next opto
-% vr.maxIterBetweenOpto = eval(vr.exper.variables.maxIterBetweenOpto); % how many iteration to wait until giving next opto
-vr.optoTriggerPoint = eval(vr.exper.variables.optoTriggerPoint);
 
-% need to determine based on opto parameters how many runs of opto
-% stimulation to give 
-% vr.allOptoVoltage = reshape(repmat(vr.optoMinVoltage:vr.optoVoltageIncrement:vr.optoMaxVoltage,vr.optoRepeat,1),[],1);
-% vr.totOpto = length(vr.allOptoVoltage); % total number of opto stimulation
+vr.optoTriggerPoint = eval(vr.exper.variables.optoTriggerPoint); % time point after which opto signal can be delivered
+
 vr.nDeliveredOpto = 0; % how many have already been delivered
-% % semi-randomly determine interval in iters between optogenetic stimulation
-% vr.optoOnsetIter = cumsum(randi([vr.minIterBetweenOpto vr.maxIterBetweenOpto],[vr.totOpto 1]));
 
-% fprintf("%i inhibitions scheduled, rangeing from %.2f to %.2f mV in %.2f mV increment \t\n",vr.totOpto,vr.optoMinVoltage,vr.optoMaxVoltage,vr.optoVoltageIncrement)
-% fprintf("First inhibition is iter %i\n",vr.optoOnsetIter(1));
-% 
+fprintf("Max voltage is %.2f mV, with %.2f s ramp up, %.2f s sustained, and %.2f s ramp down ",vr.optoMaxVoltage,vr.optoRampUpDur,vr.optoLightDur,vr.optoRampDownDur)
+end
