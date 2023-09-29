c2310cc0ee0ea68cf0a19d5b9513e6291b9e0294
xy103 <xintong.cindy.yuan@gmail.com>
2023-09-29 10:40:36 -0400

    simplified checkMaze_opto_start conditions

--- experiments/functions/checkMaze_opto_start.m
+++ experiments/functions/checkMaze_opto_start.m
@@ -6,14 +6,14 @@ if ~isempty(vr.Switches) % a switch has occured
     % only consider starting if within nTrialsContinuousOpto post switching
     if vr.numTrialSinceSW < vr.nTrialsContinuousOpto 
         % opto is not on yet but we have started a reward delay and have not received visual feedback
-        if  (vr.inRewardZone == 1) && ~vr.hasFeedback && (vr.optoOn == 0)
+        if  (vr.inRewardZone == 1) && (vr.optoOn == 0)
             vr.optoOn = 1; % turn opto on
             vr.optoStartTime = tic; % start time of current opto 
 %             vr.optoOnSec = toc(vr.optoStartTime); % time in seconds since the current opto stimulation onset
             vr.nDeliveredOpto = vr.nDeliveredOpto+1;
             vr = checkForOptoDelivery_SW(vr); % call once to start outputting
             % print out update
-            fprintf("Wait for feedback & start ramping up for light inhibition #%i,\t max voltage %.1f mV\t\n",vr.nDeliveredOpto,vr.optoMaxVoltage)
+            fprintf("**Wait for feedback & ramping up for light inhibition #%i\n",vr.nDeliveredOpto)
         end
     end
 end
