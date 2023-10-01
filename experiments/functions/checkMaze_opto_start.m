2a838c910cc295830632191cfdac7f4d6a27e928
Cindy <xyuan@g.harvard.edu>
2023-10-01 18:51:17 -0400

    fixed opto starting during maze end

--- experiments/functions/checkMaze_opto_start.m
+++ experiments/functions/checkMaze_opto_start.m
@@ -4,9 +4,9 @@ function vr = checkMaze_opto_start(vr)
 if ~isempty(vr.Switches) % a switch has occured
 
     % only consider starting if within nTrialsContinuousOpto post switching
-    if vr.numTrialSinceSW < vr.nTrialsContinuousOpto 
+    if vr.numTrialSinceSW < vr.nTrialsContinuousOpto-1
         % opto is not on yet but we have started a reward delay and have not received visual feedback
-        if  (vr.inRewardZone == 1) && (vr.optoOn == 0)
+        if  (vr.inRewardZone == 1) && ~vr.optoOn && (vr.inITI~=1) % waiting for feedback but not in iti yet
             vr.optoOn = 1; % turn opto on
             vr.optoStartTime = tic; % start time of current opto 
 %             vr.optoOnSec = toc(vr.optoStartTime); % time in seconds since the current opto stimulation onset
