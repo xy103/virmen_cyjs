37fefa1698ef1ef019e89819107070c94f2e6686
xy103 <xintong.cindy.yuan@gmail.com>
2023-10-06 11:49:59 -0400

    added velocity by postion plots for is_maze_opto

--- experiments/functions/opto_summary_visualization.m
+++ experiments/functions/opto_summary_visualization.m
@@ -18,20 +18,20 @@ normal_colors = paired_map(2:2:end,:); % lighter colors of matched trial types
 world_names = {'WR', 'WL','BR','BL'};
 
 % Collect useful data together
-maze_len = str2num(parameters.variables.floorLength) + str2num(parameters.variables.funnelLength);
+maze_len = str2double(parameters.variables.floorLength) + str2double(parameters.variables.funnelLength);
 rewardLength = 5 + maze_len;
-ramp_up_dur = str2num(parameters.variables.optoRampUpDur);
-ramp_down_dur = str2num(parameters.variables.optoRampDownDur);
-ramp_sus_dur = str2num(parameters.variables.optoLightDur);
+ramp_up_dur = str2double(parameters.variables.optoRampUpDur);
+ramp_down_dur = str2double(parameters.variables.optoRampDownDur);
+ramp_sus_dur = str2double(parameters.variables.optoLightDur);
 opto_dur = ramp_up_dur + ramp_down_dur + ramp_sus_dur;
 
 ts = cumsum(sessionData(10,:)); % timestamp
 iter_trial = sessionData(end,:);
 % opto start and end should be the same if ITI opto
-opto_start_trials = sessionData(end,optoData.optoStartIter);
+% opto_start_trials = sessionData(end,optoData.optoStartIter);
 opto_end_trials = sessionData(end,optoData.optoEndIter); % opto always end on the trial we want to perturb
 opto_start_tm = ts(optoData.optoStartIter);
-opto_end_tm = ts(optoData.optoEndIter);
+% opto_end_tm = ts(optoData.optoEndIter);
 % all trials
 all_trials = unique(iter_trial);
 normal_trials = all_trials(~ismember(all_trials,opto_end_trials));
@@ -60,21 +60,31 @@ end
 
 lat_vel = sessionData(2,:);
 forw_vel = sessionData(3,:);
+forw_pos = sessionData(6,:); % may need to cap at 205
 tm_bin_size = .05;
 tm_bin_edges = -tm_bin_size/2:tm_bin_size:opto_dur+tm_bin_size/2;
 tm_bin_ctrs = tm_bin_edges(1:end-1)+tm_bin_size/2;
+start_pos=10; end_pos = 205;
+pos_bin_size = 5;
+pos_bin_edges = -pos_bin_size/2:pos_bin_size:end_pos+pos_bin_size/2;
+pos_bin_ctrs = pos_bin_edges(1:end-1)+pos_bin_size/2;
 
 %%
 % we already know when opto starts and ends (in iters) from optoData
 title_options = {'Lateral','Forward'};
+opto_names = {'Normal','Opto'};
 
-f = figure('Position', [300 200 1000 700]);
+n_col = 3+2*is_maze_opto;
+f = figure('Position', [300 200 900+is_maze_opto*2*300 750]); % if is_maze_opto allow space for two extra columns
+ax_tm = gobjects(6,1); % shared by both plots
+
 if is_maze_opto
-    sgtitle('Maze Inhibition')
+    sgtitle({'Maze Inhibition','Opto ramps up in the previous ITI for 0.5s'})
+    ax_pos = gobjects(6,1);
 else
-    sgtitle('ITI Inhibition')
+    sgtitle({'ITI Inhibition','Opto ramps up while waiting for visual feedback for 0.5s'})
 end
-ax = gobjects(6,1);
+
 % we want to plot forward and lateral velocity with opto on and off (x axis is time)
 for vel_ind = 1:2
     if vel_ind ==1
@@ -83,61 +93,64 @@ for vel_ind = 1:2
         vel_touse = forw_vel;
     end
 
-    ax(vel_ind) = subplot(3,3,vel_ind);hold on
-    % plotting trial by trial
-    opto_tm_pts = {};
-    opto_vel = {};
-    normal_tm_pts = {};
-    normal_vel = {};
-    for j = 1:4
-        opto_tm_pts{j} = [];
-        opto_vel{j} = [];
-        normal_tm_pts{j} = [];
-        normal_vel{j} = [];
-    end
-    for i = 1:length(optoData.optoStartIter)
-        relevant_ind = optoData.optoStartIter(i):optoData.optoEndIter(i);
-        temp_tm = ts(relevant_ind)-opto_start_tm(i);
-        temp_vel = vel_touse(relevant_ind);
-        this_world = mod(opto_world_type(i),4);
-        if this_world ==0
-            this_world = 4;
+    for is_opto = 0:1
+        save_tm_pts = {};
+        save_vel = {};
+        for j = 1:4
+            save_tm_pts{j} = [];
+            save_vel{j} = [];
         end
-        opto_tm_pts{this_world} = [opto_tm_pts{this_world} temp_tm];
-        opto_vel{this_world} = [opto_vel{this_world} temp_vel];
-        plot(temp_tm,temp_vel,Color=opto_colors(this_world,:));
-    end
-    xline(ramp_up_dur,'--')
-    xline(ramp_up_dur+ramp_sus_dur,'--')
-    xlabel("Time (s)")
-    ylabel("Velocity (cm/s)")
-    xlim([0 opto_dur])
-    title(['Opto: ', title_options{vel_ind},' Velocity'])
 
-    % plot corresponding times when opto is off
-    ax(vel_ind+2) = subplot(3,3,vel_ind+3);hold on
-    for i = 1:length(normal_trials)
-        relevant_ind = ts>=normal_trials_comp_start_tm(i)&(ts<normal_trials_comp_start_tm(i)+opto_dur);
-        temp_tm = ts(relevant_ind)-normal_trials_comp_start_tm(i);
-        temp_vel = vel_touse(relevant_ind);
-        this_world = mod(normal_world_type(i),4);
-        if this_world == 0
-            this_world = 4;
+        if is_opto == 0
+            trials_to_plot = normal_trials;
+            reference_start_time = normal_trials_comp_start_tm;
+            relevant_world_type = normal_world_type;
+            colors_to_use = normal_colors;
+        else
+            trials_to_plot = opto_end_trials;
+            reference_start_time = opto_start_tm;
+            relevant_world_type = opto_world_type;
+            colors_to_use = opto_colors;
         end
-        normal_tm_pts{this_world} = [normal_tm_pts{this_world} temp_tm];
-        normal_vel{this_world} = [normal_vel{this_world} temp_vel];
-        plot(temp_tm,temp_vel,Color=normal_colors(normal_world_type(i),:));
+
+        ax_tm(vel_ind+is_opto*2) = subplot(3,n_col,vel_ind+n_col*is_opto);hold on
+        % plotting trial by trial
+        for i = 1:length(trials_to_plot)
+            if is_opto==0
+                relevant_ind = ts>=normal_trials_comp_start_tm(i)&(ts<normal_trials_comp_start_tm(i)+opto_dur);
+            else
+                relevant_ind = optoData.optoStartIter(i):optoData.optoEndIter(i);
+            end
+            temp_tm = ts(relevant_ind)-reference_start_time(i);
+            temp_vel = vel_touse(relevant_ind);
+            this_world = mod(relevant_world_type(i),4);
+            if this_world ==0
+                this_world = 4;
+            end
+            save_tm_pts{this_world} = [save_tm_pts{this_world} temp_tm];
+            save_vel{this_world} = [save_vel{this_world} temp_vel];
+            plot(temp_tm,temp_vel,Color=colors_to_use(this_world,:));
+        end
+        xline(ramp_up_dur,'--')
+        xline(ramp_up_dur+ramp_sus_dur,'--')
+        xlabel("Time (s)")
+        ylabel("Velocity (cm/s)")
+        xlim([0 opto_dur])
+        title([opto_names{is_opto+1},': ', title_options{vel_ind},' Velocity'])
+
+        % save opto results for mean sem plotting
+        if is_opto ==0
+            normal_tm_pts = save_tm_pts;
+            normal_vel = save_vel;
+        else
+            opto_tm_pts = save_tm_pts;
+            opto_vel = save_vel;
+        end
     end
-    xline(ramp_up_dur,'--')
-    xline(ramp_up_dur+ramp_sus_dur,'--')
-    xlabel("Time (s)")
-    ylabel("Velocity (cm/s)")
-    xlim([0 opto_dur])
-    title(['Normal: ',title_options{vel_ind},' Velocity'])
 
     % if we want to plotMeanSEM we need to bin by time
     % and separate this by world types!!!!!
-    ax(vel_ind+4) = subplot(3,3,vel_ind+6);hold on
+    ax_tm(vel_ind+4) = subplot(3,n_col,vel_ind+n_col*2);hold on
     for this_world = unique(all_world_type)
         [mean_binned_a,sem_binned_a] = binByTimeOrPos(opto_tm_pts{this_world},opto_vel{this_world},tm_bin_edges);
         plotMeanSEM(tm_bin_ctrs,mean_binned_a,sem_binned_a,opto_colors(this_world,:))
@@ -150,11 +163,81 @@ for vel_ind = 1:2
     ylabel("Velocity (cm/s)")
     xlim([0 opto_dur])
     title([title_options{vel_ind},' Velocity Mean ',char(177),' SEM'])
+
+    % TODO: plot velocity by forward position to see if effects carry
+    % over (if is_maze_opto is true)
+    if is_maze_opto
+        for is_opto = 0:1
+            save_pos = {};
+            save_pos_vel = {};
+            for j = 1:4
+                save_pos{j} = [];
+                save_pos_vel{j} = [];
+            end
+
+            if is_opto == 0
+                trials_to_plot = normal_trials;
+                relevant_world_type = normal_world_type;
+                colors_to_use = normal_colors;
+            else
+                trials_to_plot = opto_end_trials;
+                relevant_world_type = opto_world_type;
+                colors_to_use = opto_colors;
+            end
+
+            % plotting trial by trial
+            ax_pos(vel_ind+2*is_opto) = subplot(3,n_col,vel_ind+2+n_col*is_opto);hold on
+            for i = 1:length(trials_to_plot)
+                relevant_ind = iter_trial==trials_to_plot(i);
+                temp_pos = forw_pos(relevant_ind);
+                temp_vel = vel_touse(relevant_ind);
+                this_world = mod(relevant_world_type(i),4);
+                if this_world ==0
+                    this_world = 4;
+                end
+                save_pos{this_world} = [save_pos{this_world} temp_pos];
+                save_pos_vel{this_world} = [save_pos_vel{this_world} temp_vel];
+                plot(temp_pos,temp_vel,Color=colors_to_use(this_world,:));
+            end
+            xline(150,'--')
+            xlabel("Forwad Position (cm)")
+            ylabel("Velocity (cm/s)")
+            xlim([start_pos end_pos])
+            title([opto_names{is_opto+1},': ', title_options{vel_ind},' Velocity'])
+            % save opto results for mean sem plotting
+            if is_opto ==0
+                normal_pos = save_pos;
+                normal_pos_vel = save_pos_vel;
+            else
+                opto_pos = save_pos;
+                opto_pos_vel = save_pos_vel;
+            end
+        end
+
+        % if we want to plotMeanSEM we need to bin by time
+        % and separate this by world types!!!!!
+        ax_pos(vel_ind+4) = subplot(3,n_col,vel_ind+2+n_col*2);hold on
+        for this_world = unique(all_world_type)
+            [mean_binned_a,sem_binned_a] = binByTimeOrPos(opto_pos{this_world},opto_pos_vel{this_world},pos_bin_edges);
+            plotMeanSEM(pos_bin_ctrs,mean_binned_a,sem_binned_a,opto_colors(this_world,:))
+            [mean_binned_a,sem_binned_a] = binByTimeOrPos(normal_pos{this_world},normal_pos_vel{this_world},pos_bin_edges);
+            plotMeanSEM(pos_bin_ctrs,mean_binned_a,sem_binned_a,normal_colors(this_world,:))
+        end
+        xline(150,'--')
+        xlabel("Forwad Position (cm)")
+        ylabel("Velocity (cm/s)")
+        xlim([start_pos end_pos])
+        title([title_options{vel_ind},' Velocity Mean ',char(177),' SEM'])
+
+    end
+    linkaxes(ax_tm,'y');
+    if is_maze_opto
+        linkaxes(ax_pos,'y')
+    end
 end
-linkaxes(ax,'y');
 
 % also want to plot bar graph of correctness with opto on and off
-subplot(3,3,3)
+subplot(3,n_col,n_col)
 bar_x = categorical({'Normal','Opto'});
 % we always care about the correctness for the trial in which opto ends
 corr_normal_opto =  [mean(trial_rewarded(normal_trials)) mean(trial_rewarded(opto_end_trials))];
@@ -162,7 +245,7 @@ bar(bar_x,corr_normal_opto);
 box off
 ylabel("Frac correct")
 
-subplot(3,3,6) % color legend
+subplot(3,n_col,n_col*2) % color legend
 hold on
 world_labels = {};
 uniq_worlds = unique(all_world_type);
@@ -177,9 +260,6 @@ ylim([-10 0])
 yticks([])
 xticks([])
 set(gca,'visible','off')
-
-% Maybe: plot velocity by position to see if effects carry
-% over (if is_maze_opto is true)
 
 
 end
