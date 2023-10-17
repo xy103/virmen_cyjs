921861b4ec58f966938c4ce48b2e2c2e2fb7a236
Cindy Yuan <xyuan@g.harvard.edu>
2023-10-17 16:23:54 -0400

    working progress on goodperfopto

--- experiments/functions/checkGoodPerfMaze_opto_start.m
+++ experiments/functions/checkGoodPerfMaze_opto_start.m
@@ -2,28 +2,35 @@ function vr = checkGoodPerfMaze_opto_start(vr)
 % a switch must have happened, and we're now at the good performance period
 % indicated by more than .9 correct in the last 10 trials
 
-if ~isempty(vr.Switches)&& (vr.numTrialSinceSW > vr.goodPerfWindowBeforeOpto) % a switch has occured and we're at least 10 trials post it
+% a switch has occured and we're at least 10 trials post it
+if ~isempty(vr.Switches)&& (vr.numTrialSinceSW > vr.goodPerfWindowBeforeOpto) && ~vr.numTrialcurrentBlockOpto
     window2check = (vr.numTrials - vr.goodPerfWindowBeforeOpto) : vr.numTrials;
     if sum(vr.Rewards(window2check))/numel(window2check) >= vr.goodPerfFracCorrect  % condition 2)
-        if (vr.Rewards(vr.numTrials) == 1) && ~vr.numTrialcurrentBlockOpto % condition 3)
+        if (vr.Rewards(vr.numTrials) == 1) % condition 3)
             vr.numTrialcurrentBlockOpto = 1;
         end
     end
 end
 
-if vr.numTrialcurrentBlockOpto > 0
+% only deliver opto if the rule has not changed since the last opto trial
+if vr.numTrialcurrentBlockOpto > 0 
     % only consider starting if within nTrialsContinuousOpto post switching
     if vr.numTrialcurrentBlockOpto <= vr.nTrialsContinuousOpto
-        % opto is not on yet but we have started a reward delay and have not received visual feedback
-        if  (vr.inRewardZone == 1) && ~vr.optoOn && (vr.inITI~=1) % waiting for feedback but not in iti yet
-            vr.optoOn = 1; % turn opto on
-            vr.optoStartTime = tic; % start time of current opto 
-            vr.optoStartIter = [vr.optoStartIter vr.totIterations]; % record which iter had opto starting
-            vr.nDeliveredOpto = vr.nDeliveredOpto+1;
-            vr.numTrialcurrentBlockOpto = vr.numTrialcurrentBlockOpto +1;
-            vr = checkForOptoDelivery_SW(vr); % call once to start outputting
-            % print out update
-            fprintf("**Good performance ITI opto: wait for feedback & ramping up for light inhibition #%i\n",vr.nDeliveredOpto)
+        if vr.numTrialSinceSW > vr.goodPerfWindowBeforeOpto
+            % opto is not on yet but we have started a reward delay and have not received visual feedback
+            if  (vr.inRewardZone == 1) && ~vr.optoOn && (vr.inITI~=1) % waiting for feedback but not in iti yet
+                vr.optoOn = 1; % turn opto on
+                vr.optoStartTime = tic; % start time of current opto 
+                vr.optoStartIter = [vr.optoStartIter vr.totIterations]; % record which iter had opto starting
+                vr.nDeliveredOpto = vr.nDeliveredOpto+1;
+                vr.numTrialcurrentBlockOpto = vr.numTrialcurrentBlockOpto +1;
+                vr = checkForOptoDelivery_SW(vr); % call once to start outputting
+                % print out update
+                fprintf("**Good performance ITI opto: wait for feedback & ramping up for light inhibition #%i\n",vr.nDeliveredOpto)
+            end
+        else
+            fprintf("**Stopping opto trials early because of new switch\n")
+            vr.numTrialcurrentBlockOpto = 0; % reset to 0
         end
     else % already past nTrialContinuousOpto
         vr.numTrialcurrentBlockOpto = 0; % reset to 0
