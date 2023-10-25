2b3c0e9bde245c78adecfac44361eb4f18f2de03
Cindy Yuan <xyuan@g.harvard.edu>
2023-10-25 19:47:48 -0400

    functional entire cue period opto

--- experiments/functions/checkForOptoDelivery_Cue.m
+++ experiments/functions/checkForOptoDelivery_Cue.m
@@ -6,35 +6,34 @@ if vr.optoOn % indicator for opto light
     vr.optoOnSec = toc(vr.optoStartTime);% update how much time opto has been on
     % determine output voltage based on how much time has eplased since
     % light was turned on
-    if vr.optoOnSec < vr.optoRampUpDur % ramp up
+
+    if vr.optoOnSec <= vr.optoRampUpDur % ramp up
         vr.optoOutVoltage = (1/vr.optoRampUpDur * vr.optoOnSec)*vr.optoMaxVoltage;
-    elseif vr.optoOnSec < vr.maxOptoTmBeforeRampDown % has not exceed max time allowed for opto yet
-        if vr.position(2) < vr.floorLength % sustained period
-            vr.optoOutVoltage = vr.optoMaxVoltage;
-        else %  position exceed stem
-            if vr.rampDownStartSec==0 % start ramp down if not already started
-                vr.rampDownStartSec = tic; % keep track of when ramp down started 
+    else % past ramp up time
+        if vr.rampDownStartSec==0 % in maze, have not entered ramp down
+            if (vr.position(2) < vr.floorLength)&&(vr.optoOnSec < vr.maxOptoTmBeforeRampDown)% sustained period
+                vr.optoOutVoltage = vr.optoMaxVoltage;
+            elseif vr.inITI==0 % position or time condition violated
+                vr.rampDownStartSec = tic;
+                if vr.position(2) > vr.floorLength
+                    fprintf("**Ramping light down past stem after %.1f s\n",vr.optoOnSec)
+                else
+                     fprintf("**Ramping light down past max time after %.1f s\n",vr.optoOnSec)
+                end
+                vr.optoOutVoltage = (1-1/vr.optoRampDownDur * toc(vr.rampDownStartSec))*vr.optoMaxVoltage;
             end
-            vr.optoOutVoltage = (1-1/vr.optoRampDownDur * toc(vr.rampDownStartSec))*vr.optoMaxVoltage;
+        else % vr.rampDownStartSec>0 
+            if toc(vr.rampDownStartSec)<vr.optoRampDownDur
+                vr.optoOutVoltage = (1-1/vr.optoRampDownDur * toc(vr.rampDownStartSec))*vr.optoMaxVoltage;
+            else %if (vr.rampDownStartSec>0) && (toc(vr.rampDownStartSec)>vr.optoRampDownDur)
+                fprintf("**Light off after %.1f s\n",vr.optoOnSec)
+                vr.optoOutVoltage = 0;
+                vr.optoOn = 0; % turn opto off
+                vr.optoOnSec = 0; % default to 0 since nan gives issue with comparison
+                vr.rampDownStartSec = 0;
+                vr.optoEndIter = [vr.optoEndIter vr.totIterations]; % collect iter when opto ends
+            end
         end
-    elseif vr.optoOnSec >= vr.maxOptoTmBeforeRampDown
-        % start ramp down if not already
-        if vr.rampDownStartSec==0 % start ramp down if not already started
-            vr.rampDownStartSec = tic; % keep track of when ramp down started 
-        end
-            vr.rampDownStartSec==0 && ((vr.optoOnSec >= vr.maxOptoTmBeforeRampDown)||(vr.position(2) > vr.floorLength)) % have not started ramp down and need to
-        % start ramping down after max time has past or reached funnel
-        vr.rampDownStartSec = tic; % keep track of when ramp down started 
-        vr.optoOutVoltage = (1-1/vr.optoRampDownDur * toc(vr.rampDownStartSec))*vr.optoMaxVoltage;
-    elseif vr.rampDownStartSec~=0 && toc(vr.rampDownStartSec)<vr.optoRampDownDur % ramp down has already started and not ended yet
-        vr.optoOutVoltage = (1-1/vr.optoRampDownDur * toc(vr.rampDownStartSec))*vr.optoMaxVoltage;
-    else
-        fprintf("**Light off after %.1f s\n",vr.optoOnSec)
-        vr.optoOutVoltage = 0;
-        vr.optoOn = 0; % turn opto off
-        vr.optoOnSec = 0; % default to 0 since nan gives issue with comparison
-        vr.rampDownStartSec = 0;
-        vr.optoEndIter = [vr.optoEndIter vr.totIterations]; % collect iter when opto ends
     end
     outputSingleScan(vr.opto_ao,vr.optoOutVoltage)
 end
